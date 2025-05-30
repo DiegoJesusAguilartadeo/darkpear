56eb939bdc79c767d956fda35c2bf674c271205d
diegoqguilar <jeususlanz@gmail.com>
2025-05-30 02:17:35 -0300

    darkpear v.1.6.2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "html")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Conexión a MySQL
 const mysql = require("mysql2");
